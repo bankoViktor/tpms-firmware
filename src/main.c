0f9ed9d95a4e1ed224889db40954f3a2807fb55f
Banko Viktor <bankviktor14@gmail.com>
2025-01-09 09:24:27 +0300

    Init IDF repo

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ ********************************************************************************
+ * @file    main.c
+ * @author  Viktor Banko S. (bankviktor14@mail.com)
+ * @date    09.01.2025
+ * @brief   Entry point of the application.
+ ********************************************************************************
+ */
+
+#include <stdint.h>
+
+//constexpr float FIRMWARE_VERSION = 0.1;
+//constexpr uint32_t UART_BAUDRATE = 115200;
+
+void app_main()
+{
+}
